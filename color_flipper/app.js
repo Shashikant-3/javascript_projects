5b542383dbf86aeb139fff612c907ed7d9f43a54
Shashikant-3 <66629228+Shashikant-3@users.noreply.github.com>
2022-07-19 10:15:59 +0530

    Update app.js

--- color_flipper/app.js
+++ color_flipper/app.js
@@ -7,7 +7,7 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", function () {
   //get random number b/w 0-3 colors[3]
   const randomNumber = getRandomNumber();
-  console.log(randomNumber);
+  //console.log(randomNumber);
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
